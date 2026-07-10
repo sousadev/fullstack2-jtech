c58b58d2c0f94bb5555aadba4d5acf7681b65c9d
Italo Sousa <39159083+sousadev@users.noreply.github.com>
2026-07-10 08:36:58 -0300

    Refactor TaskGroup to include tasks and change id type
    Updated TaskGroup class to include tasks list and modified id type.

--- jtech-tasklist-backend/src/main/java/br/com/jtech/tasklist/application/core/domains/TaskGroup.java
+++ jtech-tasklist-backend/src/main/java/br/com/jtech/tasklist/application/core/domains/TaskGroup.java
@@ -2,16 +2,37 @@ package br.com.jtech.tasklist.application.core.domains;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskGroup {
 
-    public UUID id;
+    public String id;
     public String name;
     public String description;
     public LocalDateTime createdAt;
-    public TaskGroup() {
+    public List<Task> tasks;
+
+    public static TaskGroup of(TaskTGroupRequest request) {
+        return TaskGroup.builder()
+                        .id(request.id())
+                        .name(request.name())
+                        .description(request.description())
+                        .build();
     }
-    public TaskGroup(UUID id, String name, String description,  LocalDateTime createdAt) {
-        this.id = id;
+
+    public statid TaskGroup of(TaskGroupEntity entity) {
+        return TaskGroup.builder()
+                        .id(entity.getId().toString())
+                        .name(entity.getName())
+                        .description(entity.getDescription())
+                        .createdAt(entity.getCreatedAt())
+                        .build();
     }
+
+    public TaskGroupEntity 
 }
